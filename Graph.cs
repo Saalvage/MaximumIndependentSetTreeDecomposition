9967bb64b566f3ef3af998d273cab08962feba3c
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-01-27 04:36:19 +0100

    Revert "Use `HashSet`s to represent graphs"
    This reverts commit 11dfba0e4d834ce5ab2d7b4a1ef917e7e7d90134.

--- Graph.cs
+++ Graph.cs
@@ -5,25 +5,34 @@ namespace MaximumIndependentSetTreeDecomposition;
 // These classes are not general purpose, their methods are idiosyncratic.
 
 // This graph representation is effectively like a tree.
-public record UniDirGraph(IReadOnlySet<int>[] NodeEdges, bool IsBackwards = false) {
-    public static async Task<UniDirGraph> CreateFromEdges(int nodeCount, IAsyncEnumerable<(int, int)> edges, bool reverse = false) {
-        var nodeEdges = new HashSet<int>[nodeCount];
-        Initialize();
+public record UniDirGraph(int[] Nodes, int[] Edges, bool IsBackwards = false) {
+    public static async Task<UniDirGraph> CreateFromEdges(int nodeCount, int maxEdgeCount, IAsyncEnumerable<(int, int)> orderedEdges, bool reverse = false) {
+        var nodes = new int[nodeCount + 1];
+        // Having space for more edges than necessary is no problem because they are simply not referenced.
+        var edges = new int[maxEdgeCount];
+        var prevNode = 0;
+        await foreach (var (from, to) in orderedEdges) {
+            Debug.Assert(from < to ^ reverse, $"Edge is not ordered correctly {from}, {to}!");
+            if (prevNode != from) {
+                Debug.Assert(prevNode < from, $"Nodes are not ordered correctly ({from} after {prevNode})!");
 
-        await foreach (var (from, to) in edges) {
-            nodeEdges[from].Add(to);
+                MoveTo(from);
+                prevNode = from;
+            }
+            edges[nodes[from + 1]++] = to;
         }
+        MoveTo(nodes.Length - 2);
 
-        return new(nodeEdges, reverse);
+        return new(nodes, edges, reverse);
 
-        void Initialize() {
-            foreach (ref var node in nodeEdges.AsSpan()) {
-                node = [];
+        void MoveTo(int end) {
+            for (var i = prevNode + 1; i <= end; i++) {
+                nodes[i + 1] = nodes[i];
             }
         }
     }
 
-    public IReadOnlySet<int> GetEdges(int node) => NodeEdges[node];
+    public ReadOnlySpan<int> GetEdges(int node) => Edges.AsSpan()[Nodes[node]..Nodes[node + 1]];
 
     public bool HasEdge(int a, int b) {
         if (a > b ^ IsBackwards) {
@@ -36,18 +45,18 @@ public record UniDirGraph(IReadOnlySet<int>[] NodeEdges, bool IsBackwards = fals
     public int FindLeaf(int node) {
         while (true) {
             var edges = GetEdges(node);
-            if (edges.Count == 0) {
+            if (edges.Length == 0) {
                 return node;
             }
-            node = edges.First();
+            node = edges[0];
         }
     }
 
     public async Task<BiDirGraph> CreateBiDir() {
-        return new(this, await UniDirGraph.CreateFromEdges(NodeEdges.Length, GetReverseEdges(), true));
+        return new(this, await UniDirGraph.CreateFromEdges(Nodes.Length - 1, Nodes[^1], GetReverseEdges(), true));
 
         async IAsyncEnumerable<(int, int)> GetReverseEdges() {
-            for (var i = 0; i < NodeEdges.Length; i++) {
+            for (var i = 0; i < Nodes.Length - 1; i++) {
                 for (var j = 0; j < i; j++) {
                     if (HasEdge(j, i)) {
                         yield return (i, j);
@@ -62,8 +71,8 @@ public record UniDirGraph(IReadOnlySet<int>[] NodeEdges, bool IsBackwards = fals
 // Maintaining both graphs like this gives us an implicitly rooted tree.
 public record BiDirGraph(UniDirGraph ForwardGraph, UniDirGraph BackwardGraph) {
     public bool IsValid() {
-        for (var i = 0; i < ForwardGraph.NodeEdges.Length; i++) {
-            for (var j = 0; j < BackwardGraph.NodeEdges.Length; j++) {
+        for (var i = 0; i < ForwardGraph.Nodes.Length - 1; i++) {
+            for (var j = 0; j < BackwardGraph.Nodes.Length - 1; j++) {
                 if (ForwardGraph.HasEdge(i, j) != BackwardGraph.HasEdge(i, j)) {
                     return false;
                 }
