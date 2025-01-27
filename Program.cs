3840fc1e9e43b78e41ea0056431c5b9114958216
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-01-27 04:33:45 +0100

    Repeated iterations for benchmarking

--- Program.cs
+++ Program.cs
@@ -7,19 +7,33 @@ string ReadInput() {
     return Console.ReadLine()!;
 }
 
+const int ITERATIONS = 100;
+var dic = new Dictionary<string, TimeSpan>();
+
 if (Directory.Exists(path)) {
-    foreach (var file in Directory.EnumerateFiles(path, "*.gr")) {
-        await Solve(file);
+    for (var i = 0; i < ITERATIONS; i++) {
+        foreach (var file in Directory.EnumerateFiles(path, "*.gr")) {
+            await Solve(file);
+        }
     }
 } else {
     await Solve(path);
 }
 
-static async Task Solve(string file) {
+foreach (var (file, time) in dic) {
+    Console.WriteLine($"Average time for {Path.GetFileName(file)} was {time.TotalMilliseconds / ITERATIONS}ms");
+}
+
+async Task Solve(string file) {
     var (graph, bags, bagGraph) = await Input.Load(file);
     var start = DateTimeOffset.UtcNow;
     var solver = new Solver(graph, bags, bagGraph);
     var solution = solver.Solve();
     var time = DateTimeOffset.UtcNow - start;
     Console.WriteLine($"Calculated graph {Path.GetFileName(file)}'s maximum independent vertex set size to be {solution} in {time.TotalMilliseconds}ms");
+    if (dic.ContainsKey(file)) {
+        dic[file] += time;
+    } else {
+        dic.Add(file, time);
+    }
 }
